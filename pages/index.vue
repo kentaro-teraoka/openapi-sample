a60aa48b054b463fbbac6a78e78c73534b9a95ae
k-teraoka <k-teraoka@labol.co.jp>
2025-12-02 10:35:33 +0900

    使わないLinkを非表示

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
     <ul>
-      <li>
+      <!-- <li>
         <nuxt-link to="/openapi-generator-cli">openapi-generator-cli</nuxt-link>
-      </li>
+      </li> -->
       <li>
         <nuxt-link to="/openapi-typescript">openapi-typescript</nuxt-link>
       </li>
-      <li>
+      <!-- <li>
         <nuxt-link to="/openapi-typescript-codegen">openapi-typescript-codegen</nuxt-link>
       </li>
       <li>
         <nuxt-link to="/swagger-typescript-api">swagger-typescript-api</nuxt-link>
-      </li>
+      </li> -->
     </ul>
   </div>
 </template>
